012b6b834d9ecd7813ea520a83a754eb5f4cd7c1
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-09 14:22:29 +0000

    today we finish variable and datatypes

---
+++ jsbegin/02_variables.js
@@ -0,0 +1,25 @@
+const studentName = "yash"
+let studentAge = 19
+const studentPN = 897601234
+let studentCity = "mumbai"
+
+console.log(studentName);
+console.table([studentAge, studentPN, studentCity])
+
+
+//*  use "const" when u want to keep the value fixed for an variable
+//*  use "let"   if u want to change the value of variable now or in the future
+//*  use "console.table"  in order to print multiple variables value in form of table
+//*  use "log" shortcut to print anythng
+
+
+//*  in this codeblock u are taking data from the student
+
+
+
+
+
+
+
+
+
---
+++ jsbegin/03_datatype.js
@@ -0,0 +1,19 @@
+const name ="yash tiwari"
+let age = 19
+const degree ="electronics and computer science"
+
+// number => range 2 to the power of 52
+ // string => always within ""
+ // boolean => true/false
+// null => standalone value
+   // symbol => unique
+ 
+
+console.log(typeof age);
+
+//* use "console.log(typeof-----)" to know about the datatype of the mention variable.
+
+
+
+
+    
