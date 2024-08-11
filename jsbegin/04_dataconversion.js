9d6d793b53c0766fbbb73f9a7ee48c5ecb42244c
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-11 05:32:12 +0000

    learn dataconversion very well

---
+++ jsbegin/04_dataconversion.js
@@ -0,0 +1,24 @@
+ const name = "yash tiwari"
+ let age = "19"
+const  EndSemScore = 7.95
+const RollNo=54
+const Pass = true
+
+// imagine this is the data u are getting from the frontend, 
+// may be while seeing u will know the datatype but it can be in different format
+
+console.log(age)
+console.log(typeof age);
+let AgeInNumber = Number(age)  
+console.log(AgeInNumber)    
+console.log(typeof AgeInNumber)
+
+// declare a  new variable or the conversion 
+// syntax "DTuwanttoconvert(variableofthedata)"
+
+console.log(Pass)
+console.log(typeof Pass)
+let PasInString = String(Pass)
+console.log(PasInString)
+console.log(typeof PasInString)
+// ANOTHER EXAMPLE
