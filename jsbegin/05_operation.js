502d55abde33467b31fb300d212c508722a785e0
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-11 07:12:49 +0000

    operation seen in js done

---
+++ jsbegin/05_operation.js
@@ -0,0 +1,29 @@
+
+
+console.log("2" + 3 + 1)
+//* here if string come frist then it convets the remaining value in str
+
+console.log(1 +2 + "4")
+//* here the operation is done frist as they come frist 
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+//* for increment operation 
+
+
+
+console.log((1+2) % (3*8))
+//* try to organise ur operation
+
+
