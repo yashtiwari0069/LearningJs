a64710743cf9ad59b3242449737c1df759447e22
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-12 15:17:48 +0000

    done with datacomparison only one topic today

---
+++ jsbegin/06_datatypecomparison.js
@@ -0,0 +1,37 @@
+//* normal comparison btw similar datatype
+console.log(5>1);
+console.log(5==1);
+ console.log(5<1);
+ console.log(5!=1);
+
+ // the output will be in the form of true/false(boolean)
+
+
+
+ //* now comparison btw different datatype
+  console.log("5" > 1);  // here it is btw string and number
+  console.log("5" == 5);
+  
+  //* here output maybe in your terms but avoid this
+
+  console.log("5" === 5);  // here using 3 equal sign it does strict checking
+
+
+ // console.log(null > 0);
+ // console.log(null < 0);
+ // console.log(null = 0);
+  
+  
+  //* avoid such type of comparison also with undefine datatype
+
+  
+  
+
+
+
+
+ 
+ 
+ 
+ 
+
