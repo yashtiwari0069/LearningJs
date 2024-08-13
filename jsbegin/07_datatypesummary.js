3796804a12e28938b661cf5eee62c8ad3ea1cf45
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-13 15:49:45 +0000

    done with the summary of dt

---
+++ jsbegin/07_datatypesummary.js
@@ -0,0 +1,52 @@
+// there are 2 types of datatype
+
+//* primitive datatype : string, number, boolean, null, underfined, bigin, symbol
+
+
+// ex for symbol
+const userID1 = Symbol('123')
+const userID2 = Symbol('123')
+console.log(userID1 === userID2);  // using strict checking
+
+// simply it wraps the value and make it unique
+
+
+//
+//* non primitive datatype : arrays, objects, functions
+
+// here just gonna see the basic
+
+// FOR ARRAY \\
+//const MyFriends = ["pratik","krish","dheeraj"]
+//console.log(MyFriends);
+
+//FOR OBJECTS \\
+//const obj ={
+   // name:"yash"
+ //   age = 19
+//}
+
+//let make = Function(){
+  //  console.log("hello world");
+    
+//}
+
+
+///* can ask for interview IMP
+
+//Return type of variables in JavaScript
+//1) Primitive Datatypes
+    //   Number => number
+    //   String  => string
+    //   Boolean  => boolean
+    //   null  => object
+    //   undefined  =>  undefined
+    //   Symbol  =>  symbol
+   //    BigInt  =>  bigint
+
+//2) Non-primitive Datatypes
+ //      Arrays  =>  object
+ //      Function  =>  function
+ //      Object  =>  object
+
+
