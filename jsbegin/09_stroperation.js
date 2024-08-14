de277ed6390a649c225325def664295bebc45e4f
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-14 16:19:36 +0000

    .

---
+++ jsbegin/09_stroperation.js
@@ -0,0 +1,6 @@
+const name = "yash"
+const surname = "tiwari"
+console.log(name + surname);  //   nood way
+
+console.log(`my name is ${name} and my surname is ${surname} i am 19 yers old`); // inserting the variables pro way
+
