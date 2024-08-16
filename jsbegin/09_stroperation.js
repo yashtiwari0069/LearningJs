35c99ebfe414245545a44aa385f822ce594e2881
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-16 03:39:04 +0000

    finally understood

--- jsbegin/09_stroperation.js
+++ jsbegin/09_stroperation.js
@@ -1,6 +1,12 @@
-const name = "yash"
-const surname = "tiwari"
-console.log(name + surname);  //   nood way
+//const name = "yash"
+//const surname = "tiwari"
+//console.log(name + surname);  //   nood way
 
-console.log(`my name is ${name} and my surname is ${surname} i am 19 yers old`); // inserting the variables pro way
+//console.log(`my name is ${name} and my surname is ${surname} i am 19 yers old`); // inserting the variables pro way
 
+const username = new String('yash')  // use this way to in console on brower
+                                     // to know about the all different methods
+
+//* try to practice all the methods while refering mdn docs 
+//* daily 2 methods
+                                    
