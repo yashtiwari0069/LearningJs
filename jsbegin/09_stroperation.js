5a59ebc6f592ecd3ea3fb4a845ceae2806a10f8e
Yash_Tiwari <139530218+yashtiwari0069@users.noreply.github.com>
2024-08-16 06:23:13 +0000

    pd

--- jsbegin/09_stroperation.js
+++ jsbegin/09_stroperation.js
@@ -9,4 +9,32 @@ const username = new String('yash')  // use this way to in console on brower
 
 //* try to practice all the methods while refering mdn docs 
 //* daily 2 methods
+
+
+console.log(username.charAt(1)); 
+  // it is used to tell which  char is  at that position
+
+const sent = "today is friay and i am very happy"
+console.log(sent.slice(20));  // here we giving the starting value
+console.log(sent.slice(4,20)); 
+// here we give the start and end value to slice 
+// here cutting was done from the front
+
+console.log(sent.slice(-4));
+console.log(sent.charAt(0));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                                     
